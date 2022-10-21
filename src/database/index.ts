76796f6b57bc7f1731f7d5f03a999e132b704159
Gabriel Borel <biel_borel@hotmail.com>
2022-10-21 18:10:56 -0300

    Fix

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,4 +1,5 @@
 import { DataSource } from "typeorm";
+import { Survey } from "../models/Survey";
 import { User } from "../models/User";
 import { CreateUsers1666356344930 as CreateUsers } from "./migrations/1666356344930-CreateUsers";
 import { CreateSurveys1666384351188 as CreateSurveys } from "./migrations/1666384351188-CreateSurveys";
@@ -7,7 +8,7 @@ export const dataSource = new DataSource({
   type: "sqlite",
   database: "src/database/database.sqlite",
   migrations: [CreateUsers, CreateSurveys],
-  entities: [User],
+  entities: [User, Survey],
 });
 
 dataSource
