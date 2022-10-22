8b540ec8302e3535806af6d15826fcd5b97a9bbd
Gabriel Borel <biel_borel@hotmail.com>
2022-10-22 17:27:45 -0300

    SurveysUsers migration

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,8 +1,10 @@
 import { DataSource } from "typeorm";
 import { Survey } from "../models/Survey";
+import { SurveyUser } from "../models/SurveyUser";
 import { User } from "../models/User";
 import { CreateUsers1666356344930 as CreateUsers } from "./migrations/1666356344930-CreateUsers";
 import { CreateSurveys1666384351188 as CreateSurveys } from "./migrations/1666384351188-CreateSurveys";
+import { CreateSurveysUsers1666392412398 as SurveysUsers } from "./migrations/1666392412398-CreateSurveysUsers";
 
 export const dataSource = new DataSource({
   type: "sqlite",
@@ -10,6 +12,6 @@ export const dataSource = new DataSource({
     process.env.NODE_ENV === "test"
       ? "src/database/database.test.sqlite"
       : "src/database/database.sqlite",
-  migrations: [CreateUsers, CreateSurveys],
-  entities: [User, Survey],
+  migrations: [CreateUsers, CreateSurveys, SurveysUsers],
+  entities: [User, Survey, SurveyUser],
 });
---
+++ src/database/migrations/1666392412398-CreateSurveysUsers.ts
@@ -0,0 +1,40 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateSurveysUsers1666392412398 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "surveys_users",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "user_id",
+            type: "uuid",
+          },
+          {
+            name: "survey_id",
+            type: "uuid",
+          },
+          {
+            name: "value",
+            type: "number",
+            isNullable: true,
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("surveys_users");
+  }
+}
