b266769e74f70a096bd089dc1a6a36216f66a9c3
Gabriel Borel <biel_borel@hotmail.com>
2022-10-23 08:15:46 -0300

    Added validations with yup

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,9 +1,24 @@
 import { Request, Response } from "express";
 import { UsersRepository } from "../repositories/UsersRepository";
+import * as yup from "yup";
 
 export class UserController {
   async create(req: Request, res: Response): Promise<Response> {
     const { name, email } = req.body;
+
+    const schema = yup.object().shape({
+      name: yup.string().required("Nome é obrigatório"),
+      email: yup
+        .string()
+        .email("Email inválido")
+        .required("Email é obrigatório"),
+    });
+
+    try {
+      await schema.validate(req.body, { abortEarly: false });
+    } catch (err) {
+      return res.status(400).json({ error: err });
+    }
 
     const userAlreadyExists = await UsersRepository.findByEmail(email);
 
@@ -12,7 +27,6 @@ export class UserController {
     }
 
     const user = UsersRepository.create({ name, email });
-
     await UsersRepository.save(user);
 
     return res.status(201).json(user);
